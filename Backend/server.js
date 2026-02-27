9ce19c4252d33f8245536c926d122066da3faa45
biniam7534 <biniamabuu75@gmail.com>
2026-02-27 21:08:23 +0300

    Update CORS for production

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS
-app.use(cors());
+app.use(cors({
+    origin: "https://digital-heritage-vaul.vercel.app",
+    credentials: true
+}));
 
 // Route files
 const heritage = require('./routes/heritage');
@@ -31,7 +34,6 @@ app.use('/api/v1/artifacts', artifacts);
 app.get('/', (req, res) => {
     res.send('🏛️ Digital Heritage Vault API is running...');
 });
-
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
